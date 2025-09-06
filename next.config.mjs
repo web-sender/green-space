2a5b0c809270f522c27ab4e7b910e81e15a7bd5c
web-sender <inversion.cluster__@outlook.com>
2025-09-06 11:18:35 +0300

    Non-technical edits next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	images: {
-		unoptimized: true,
-	}
+	
 };
 
 export default nextConfig;
-// https://grok.com/share/c2hhcmQtMw%3D%3D_87764dde-df60-498f-88c7-bad7fd2d7d79
